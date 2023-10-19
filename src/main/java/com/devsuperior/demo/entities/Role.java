595e3d735b18c0dff43f2c03f368e7ad4cd14291
Thiago <thiagoleonardo1995@live.com>
2023-10-19 09:47:14 -0300

    Checklist do Spring Security : UserDetails

--- src/main/java/com/devsuperior/demo/entities/Role.java
+++ src/main/java/com/devsuperior/demo/entities/Role.java
@@ -1,18 +1,17 @@
 package com.devsuperior.demo.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Objects;
 
 @Entity
 @Table(name = "tb_role")
-public class Role {
+public class Role implements GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String authority;
-
-    
 
     public Role() {}
     public Role(Long id, String authority) {
@@ -28,6 +27,7 @@ public class Role {
         this.id = id;
     }
 
+    @Override
     public String getAuthority() {
         return authority;
     }
