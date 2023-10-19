151b6f4c266ffeec0c0d68cba796fad60fd0edf3
Thiago <thiagoleonardo1995@live.com>
2023-10-19 09:34:13 -0300

    BCrypt password encoder

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,7 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com',
+    '$2a$10$VvClUy0yKMNVLNAtIVLWh.kDxAvJh0htKEbZYQOjY2Egbu6xTVbIG');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com',
+    '$2a$10$VvClUy0yKMNVLNAtIVLWh.kDxAvJh0htKEbZYQOjY2Egbu6xTVbIG');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
